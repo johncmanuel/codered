243b9c0c64b2aba004b7c212dc321b8a84e2dc93
BrandyNguyen2 <brandy.nguyen02@gmail.com>
2024-12-29 22:52:01 -0800

    Change background color

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -6,11 +6,11 @@
 
 <style>
   :global(body) {
-    background-color: #0cc9bd;
+    background-color: #20215E;
     margin: 0;
     padding: 0;
     color: rgba(255, 255, 255, 0.87);
-    background-color: #000000;
-    font-family: Arial, Helvetica, sans-serif;
+    background-color: #20215E;
+    font-family: Audiowide;
   }
 </style>
