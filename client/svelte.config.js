11f01d75db55b288cd99e90531c264318742b3b4
BrandyNguyen2 <brandy.nguyen02@gmail.com>
2025-01-24 13:42:02 -0800

    Enhanced image from svelete and add dependencies in config file

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,11 +1,12 @@
 import adapter from "@sveltejs/adapter-static";
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess(), enhancedImages()],
   kit: {
     adapter: adapter({
       precompress: false,
