c881caa158165f453795b2add6fe06c6cbdc4ccf
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-02-18 12:14:34 -0800

    add sandbox scene at lobby page (#17)

--- client/src/game/main.ts
+++ client/src/game/main.ts
@@ -1,15 +1,25 @@
 import { CodeRed as MainGame } from "./scenes/CodeRed";
 import { AUTO, Game, type Types } from "phaser";
+import { PUBLIC_NODE_ENV } from "$env/static/public";
+import { Sandbox } from "./scenes/Sandbox";
+
+export const IS_DEV = PUBLIC_NODE_ENV === "development";
+
+export const CONFIG_WIDTH = 1280;
+export const CONFIG_HEIGHT = 720;
+export const CONFIG_PARENT_CONTAINER = "game-container";
+export const CONFIG_BACKGROUND_COLOR = "#028af8";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
   type: AUTO,
-  width: 1280,
-  height: 720,
-  parent: "game-container",
-  backgroundColor: "#028af8",
-  scene: [MainGame],
+  width: CONFIG_WIDTH,
+  height: CONFIG_HEIGHT,
+  parent: CONFIG_PARENT_CONTAINER,
+  backgroundColor: CONFIG_BACKGROUND_COLOR,
+  // make sandbox first scene to load
+  scene: IS_DEV ? [Sandbox, MainGame] : [MainGame],
 };
 
 const StartGame = (parent: string) => {
