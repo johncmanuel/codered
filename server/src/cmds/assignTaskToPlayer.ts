0005cdc333414c1fef4a10943ad1df385dc01242
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-02-27 10:28:44 -0800

    wip

--- server/src/cmds/assignTaskToPlayer.ts
+++ server/src/cmds/assignTaskToPlayer.ts
@@ -1,6 +1,6 @@
 import { Command } from "@colyseus/command";
 import { CodeRedRoom } from "../Game";
-import { TaskState } from "../CodeRedState";
+import { Tasks, TaskState, MULTIPLAYER_TASKS } from "../CodeRedState";
 import { Client } from "colyseus";
 
 export class AssignTaskToPlayerCommand extends Command<
@@ -29,6 +29,43 @@ export class AssignTaskToPlayerCommand extends Command<
     player.activeTaskId = task.id;
     this.state.activeTasks.set(task.id, task);
     this.room.actualTasks.push(task);
+
+    // this.room.lobbyControlsByPlayer.forEach((ctrls, playerId) => {
+    //   console.log("Player", playerId, "controls:", ctrls);
+    // });
+    //
+    // Track players assigned to multiplayer tasks, this will be used to keep them in sync
+    if (MULTIPLAYER_TASKS.has(Tasks[task.type as keyof typeof Tasks])) {
+      console.log("Multiplayer task assigned:", task.type);
+      if (this.room.multiplayerTasksByPlayers.has(task.id)) {
+        console.warn("Multiplayer task already has players assigned:", task.id);
+      }
+      // get assigned player and player with appropiate controls
+      // if the assigned player and player with controls are same, assign a random player not yet part of the players array to help them out
+      // with this task
+      const players: Array<string> = [client.sessionId];
+      this.room.lobbyControlsByPlayer.forEach((ctrls, playerId) => {
+        const ctrlsSet = new Set();
+        ctrls.forEach((ctrl) => ctrlsSet.add(ctrl));
+        // if player already part of the players arr and has the same controls, assign a random player
+        if (players.includes(playerId) && ctrls.includes(task.control)) {
+          const availablePlayers = Array.from(this.state.players.keys()).filter(
+            (id) => !players.includes(id),
+          );
+          if (availablePlayers.length > 0) {
+            const randomPlayerId =
+              availablePlayers[Math.floor(Math.random() * availablePlayers.length)];
+            players.push(randomPlayerId);
+          }
+        } else {
+          if (ctrlsSet.has(task.control)) {
+            players.push(playerId);
+          }
+        }
+      });
+      console.log("Players assigned to multiplayer task:", players);
+      this.room.multiplayerTasksByPlayers.set(task.id, players);
+    }
 
     client.send("newTask", task);
 
