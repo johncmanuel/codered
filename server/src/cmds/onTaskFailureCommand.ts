ee3fd5ad4554dbddf13bf2344795a495bbbc148c
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-03-10 17:09:58 -0700

    fix new task not being sent after failing one

--- server/src/cmds/onTaskFailureCommand.ts
+++ server/src/cmds/onTaskFailureCommand.ts
@@ -32,6 +32,8 @@ export class OnTaskFailureCommand extends Command<
 
     this.state.activeTasks.delete(taskId);
     this.room.actualTasks = this.room.actualTasks.filter((t) => t.id !== taskId);
+    playerClientWithTask.send("taskFailed", taskId);
+    this.state.tasksDone++;
 
     console.log("Task failed by", client.sessionId, "Task type:", task.type);
     console.log("Player that task", task.type, "was assigned to:", playerClientWithTask.sessionId);
