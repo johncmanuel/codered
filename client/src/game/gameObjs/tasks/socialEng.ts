906048382fd358a7ee17e31a5d53bf898a075a1e
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2025-04-10 16:21:02 -0700

    init

---
+++ client/src/game/gameObjs/tasks/socialEng.ts
@@ -0,0 +1,10 @@
+import { Scene, GameObjects } from "phaser";
+import { Task } from "./task";
+
+export class SocialEng extends Task {
+  constructor(scene: Scene, taskId: string) {
+    super(scene, taskId);
+  }
+  start() {}
+  update() {}
+}
