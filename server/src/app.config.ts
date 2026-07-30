4432151f1bc4750841461b22b2f16ceed6f93bf9
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2026-07-30 09:52:53 -0700

    add cors for server, update packages for client, server, root

--- server/src/app.config.ts
+++ server/src/app.config.ts
@@ -1,6 +1,7 @@
 import config from "@colyseus/tools";
 import { monitor } from "@colyseus/monitor";
 import { playground } from "@colyseus/playground";
+import cors from "cors";
 
 /**
  * Import your Room files
@@ -24,6 +25,24 @@ export default config({
      * Bind your custom express routes here:
      * Read more: https://expressjs.com/en/starter/basic-routing.html
      */
+
+    const allowedOrigins =
+      process.env.NODE_ENV !== "production"
+        ? [
+            "http://localhost:8080",
+            "http://localhost:5173",
+            "http://127.0.0.1:8080",
+            "http://127.0.0.1:5173",
+          ]
+          // hardcoding this for now lol
+        : ["https://codered-snowy.vercel.app"];
+
+    app.use(
+      cors({
+        origin: allowedOrigins,
+        credentials: true,
+      }),
+    );
 
     /**
      * Use @colyseus/playground
